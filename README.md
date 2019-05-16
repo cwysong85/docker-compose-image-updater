d370cc1e631a79c6936f306ac3a78f547c25d92e
Corey Wysong <cwysong@indigital.net>
2019-05-16 18:12:48 -0400

    updated readme, added -o flag, added -v flag, added --help flag

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
 # Docker Compose Image Updater
+A simple way to update an existing docker-compose.yml file with images you provide.
+
+Managing a big docker-compose file can be tedious at times. If you're like me and are constantly updating docker-compose files across multiple machines, this tool makes life a lot easier. 
 
 ### Installation
 ```
 npm i docker-compose-image-updater -g
 ```
 
+#### Usage
+
+```
+$> dciu --help
+
+Usage: dciu [options]
+
+Options:
+  -v, --version              print dciu version
+  -u, --update-file          JSON update file to pass in
+  -y, --yaml-file            YAML file to update, typically a docker-compose.yml file
+```
+
 ### Update JSON file syntax
  - Must have a "services" object
- - Under each "services" object, you list out each service you want to update
- - Must have a "image" under your service.
+ - Must have services that match your services under your docker-compose.yml file
+ - Must have a "image" under your service
 
 Example `images-to-update.json` file:
 ```
--- bin/index.js
+++ bin/index.js
@@ -3,21 +3,53 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
 const path = require('path')
+const pkg = require('../package.json')
 
 var args = process.argv.slice(2)
 var updateFilePath = null
 var yamlFilePath = null
+var backup = true
 
 if(args.length === 0) {
     console.log('Please provide arguments --update-file /path/to/file.json & --yaml-file /path/to/yaml.yml')
     process.exit()
 }
 
-if (!args.includes('--update-file')) {
-    console.log('Please provide argument "--update-file /path/to/file.json"')
+if(args.includes('--help')) {
+    console.log(`
+    Usage: dciu [options]
+    
+    Options:
+      -v, --version              print dciu version
+      -u, --update-file          JSON update file to pass in
+      -y, --yaml-file            YAML file to update, typically a docker-compose.yml file
+      -o, --omit-backup          Do not produce a backup of the original file. Default is to backup the file e.g. docker-compose.yml.1558044602.backup`)
+    process.exit();
+}
+
+if(args.includes('-v') || args.includes('--version')) {
+    console.log(pkg.version)
+    process.exit();
+}
+
+if (args.includes('-o') || args.includes('--omit-backup')) {
+    backup = false;
+}
+
+if (!args.includes('--update-file') && !args.includes('-u')) {
+    console.log('Please provide a path to the JSON update file. Type "dciu --help" for usage.')
     process.exit()
 } else {
     var i = args.indexOf('--update-file')
+    if(i === -1) {
+        i = args.indexOf('-u')
+    }
+
+    if(i === -1) {
+        console.log('Could not get JSON update file from passed in arguments')
+        process.exit();
+    }
+
     var f = args[i+1]
 
     if(!fs.existsSync(f)) {
@@ -32,11 +64,20 @@ if (!args.includes('--update-file')) {
     }
 }
 
-if (!args.includes('--yaml-file')) {
-    console.log('Please provide argument "--yaml-file /path/to/yaml.yml"')
+if (!args.includes('--yaml-file') && !args.includes('-y')) {
+    console.log('Please provide a path to the YAML file to update. Type "dciu --help" for usage.')
     process.exit()
 } else {
     var i = args.indexOf('--yaml-file')
+    if(i === -1) {
+        i = args.indexOf('-y')
+    }
+
+    if(i === -1) {
+        console.log('Could not get YAML file from passed in arguments')
+        process.exit();
+    }
+
     var f = args[i+1]
 
     if(!fs.existsSync(f)) {
@@ -96,18 +137,40 @@ try {
     try {
         var newYaml = yaml.dump(replaceInYaml(jsonDoc, yamlDoc))
 
-        if(newYaml) {
+        if(newYaml && !backup) {
+            
+            console.log('Omitting backup!')
+
             fs.writeFile(yamlFilePath, newYaml, { encoding: 'utf-8' }, (err) => {
                 if(err) {
                     console.log('An error occurred while writing new YAML file.')
                     process.exit();
                 }
     
-                console.log('Updated "' + yamlFilePath + '"!')
+                console.log('Updated "' + yamlFilePath + '" with new values')
             })
+        } else {
+            var d = new Date();
+            var n = Math.round(d.getTime() / 1000);
+            fs.rename(yamlFilePath, `${yamlFilePath}.${n}.backup`, (err) => {
+                if (err) {
+                    throw err
+                }
+                
+                console.log(`Created backup file "${yamlFilePath}.${n}.backup"`)
+
+                fs.writeFile(yamlFilePath, newYaml, { encoding: 'utf-8' }, (err) => {
+                    if(err) {
+                        console.log('An error occurred while writing new YAML file.')
+                        process.exit()
+                    }
+        
+                    console.log('Updated "' + yamlFilePath + '" with new values')
+                })
+              });
         }
     } catch(e) {
-        throw e;
+        throw e
     }
 
 } catch (e) {
